d8f37ccbb5cfe23f2c185612f8463c237c3303fd
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-15 23:08:00 -0400

    Add onSignupCick class to signup.java Add Toast message if passwords don't match

--- app/src/main/java/com/example/chady/myapplication/SignUp.java
+++ app/src/main/java/com/example/chady/myapplication/SignUp.java
@@ -2,7 +2,10 @@ package com.example.chady.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Chady on 3/15/2018.
@@ -16,6 +19,32 @@ public class SignUp extends Activity {
         setContentView(R.layout.signup);
 
 
+    }
+
+    public void onSignupClick(View v)
+    {
+        if(v.getId() == R.id.Bsignupbutton)
+        {
+            EditText name = (EditText)findViewById(R.id.TFname);
+            EditText email = (EditText)findViewById(R.id.TFemail);
+            EditText uname = (EditText)findViewById(R.id.TFuname);
+            EditText pass1 = (EditText)findViewById(R.id.TFpass1);
+            EditText pass2 = (EditText)findViewById(R.id.TFpass2);
+
+
+            String namestr = name.getText().toString();
+            String emailstr = email.getText().toString();
+            String unamestr = uname.getText().toString();
+            String pass1str = pass1.getText().toString();
+            String pass2str = pass2.getText().toString();
+
+            if(!pass1str.equals(pass2str))
+            {
+                // pop up message
+                Toast pass = Toast.makeText(SignUp.this, "Passwords don't match!" , Toast.LENGTH_SHORT);
+                pass.show();
+            }
+        }
     }
 
 }
