d84acc6583732fa5e03e1f25e9b64e0621389d1e
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-15 22:21:03 -0400

    Add overide to Signup Java

--- app/src/main/java/com/example/chady/myapplication/SignUp.java
+++ app/src/main/java/com/example/chady/myapplication/SignUp.java
@@ -1,8 +1,21 @@
 package com.example.chady.myapplication;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by Chady on 3/15/2018.
  */
 
-public class SignUp {
+public class SignUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+
+    }
+
 }
