ef4003f903f66fd1c3f1f6d9a378070259958743
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-20 19:58:34 -0400

    Add DatabaseHelper.java Add implementation methods

---
+++ app/src/main/java/com/example/chady/myapplication/DatabaseHelper.java
@@ -0,0 +1,20 @@
+package com.example.chady.myapplication;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Chady on 3/20/2018.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
