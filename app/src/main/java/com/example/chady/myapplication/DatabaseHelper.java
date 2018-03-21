fe9821a8ce1693371d709cebce82145b46ae3518
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-20 20:30:46 -0400

    Add constructor class Add database Add string to create table

--- app/src/main/java/com/example/chady/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/chady/myapplication/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.chady.myapplication;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,13 +9,40 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION =1;
+    private static final String DATABASE_NAME = "contacts.db";
+    private static final String TABLE_NAME = "contacts";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_UNAME = "uname";
+    private static final String COLUMN_PASS = "pass";
+
+    SQLiteDatabase db;
+
+    private static final String TABLE_CREATE = "create table" + TABLE_NAME + "(id integer primary key not null auto_increment , " +
+            COLUMN_NAME + " text not null , " + COLUMN_EMAIL + "text not null , " + COLUMN_UNAME + " text not null , " + COLUMN_PASS + " text not null);";
+
+    public DatabaseHelper(Context context)
+    {
+        super (context , DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        db.execSQL(TABLE_CREATE);
+        this.db = db;
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String query = "DROP TABLE IF EXISTS" + TABLE_NAME;
+        db.execSQL(query);
+        this.onCreate(db);
+
 
     }
 }
